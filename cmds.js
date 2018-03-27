d8fcf1c870a5031b141d0f0449987cd1966ab0d8
Eros García Arroyo <eros.garcia.arroyo@alumnos.upm.es>
2018-03-27 20:20:11 +0200

    QuizV4

--- cmds.js
+++ cmds.js
@@ -42,7 +42,7 @@ const {
     //rl.prompt();
   };
 
-  const makeQuestion = (rl, text) => {
+  const makeQuestion = (socket, rl, text) => {
     return new Sequelize.Promise((resolve, reject) => {
       rl.question(colorize(text, 'red'), answer => {
         resolve(answer.trim());
